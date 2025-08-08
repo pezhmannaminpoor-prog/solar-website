8e81e971043f5c5806b1c181bffefdbaf89905ce
pezhmannaminpoor-prog <pezhmannaminpoor@gmail.com>
2025-08-08 17:44:50 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// گرفتن دکمه از صفحه
+const consultBtn = document.getElementById('consult-btn');
+
+// اضافه کردن رویداد کلیک
+consultBtn.addEventListener('click', () => {
+  alert('ممنون از درخواست شما! همکاران ما به زودی با شما تماس می‌گیرند.');
+});
